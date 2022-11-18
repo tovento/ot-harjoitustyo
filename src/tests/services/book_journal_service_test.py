825c656c48ae212ba94763b7e657984f2ac64133
tovento <92781588+tovento@users.noreply.github.com>
2022-11-18 13:22:38 +0200

    Write first test

---
+++ src/tests/services/book_journal_service_test.py
@@ -0,0 +1,19 @@
+import unittest
+from entities.book import Book
+from services.book_journal_service import BookJournalService
+
+class TestBookJournalService(unittest.TestCase):
+    def setUp(self):
+        self.service = BookJournalService()
+
+    def test_BJS_returns_correct_object(self):
+        result = self.service.add_read_book(
+                                    "1.1.2022",
+                                    "Bookname",
+                                    "Great Author",
+                                    123)
+        assert result.date == "1.1.2022"
+        assert result.name == "Bookname"
+        assert result.author == "Great Author"
+        assert result.pages == 123
+        assert result.notes == None
