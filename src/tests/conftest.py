b1720066ce6709682c6682471ad9230a6c557604
tovento <92781588+tovento@users.noreply.github.com>
2022-11-24 11:16:39 +0200

    Update conftest

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,4 +1,4 @@
-from initialize_database import initialize_database
+from initialize_database import reinitialize_database
 
 def pytest_configure():
-    initialize_database()
+    reinitialize_database()
