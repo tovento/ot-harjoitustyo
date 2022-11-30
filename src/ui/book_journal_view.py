6807153f3550b0a99b3850647fb06581f3ddb774
tovento <92781588+tovento@users.noreply.github.com>
2022-11-30 08:42:53 +0200

    Create ui view for reading list

--- src/ui/book_journal_view.py
+++ src/ui/book_journal_view.py
@@ -1,45 +1,80 @@
-from tkinter import ttk, constants
+from tkinter import ttk
 from services.book_journal_service import book_journal_service
 
 class BookJournalView:
-    """Näkymä, jossa listataan luetut kirjat."""
+    """Lukupäiväkirjan perusnäkymä, jossa on kaksi välilehteä. Ensimmäisellä välilehdellä
+    käyttäjä näkee lukemansa kirjat. Toisella välilehdellä on lukulista eli
+    kirjat, jotka käyttäjä on merkinnyt haluavansa lukea.
+    """
 
     def __init__(self, root, handle_add_read_book_view):
-        """TODO"""
+        """Luokan konstruktori. Luo lukupäiväkirjan perusnäkymän kahdella
+        välilehdellä.
+
+        Args:
+            root:
+                TKinter-elementti, jonka sisään näkymä alustetaan.
+            handle_add_read_book_view:
+                Kutsuttava funktio, joka vie käyttäjän lomakkeeseen, jolla hän
+                voi lisätä uuden luetun kirjan lukupäiväkirjaan.
+        """
 
         self._root = root
         self._handle_add_read_book_view = handle_add_read_book_view
-        self._frame = None
+        self._frame1 = None
+        self._frame2 = None
+        self._notebook = ttk.Notebook(self._root)
+        self._notebook.pack(pady=5, expand=True)
 
         self._initialize()
 
     def destroy(self):
-        self._frame.destroy()
+        """Tuhoaa näkymät."""
+        self._frame1.destroy()
+        self._frame2.destroy()
 
     def pack(self):
-        self._frame.pack(fill=constants.X)
+        """Näyttää näkymät."""
+        self._frame1.pack(fill="both", expand=True)
+        self._frame2.pack(fill="both", expand=True)
+
+        self._notebook.add(self._frame1, text="Lukupäiväkirja")
+        self._notebook.add(self._frame2, text="Lukulista")
     
     def _initialize(self):
-        self._frame = ttk.Frame(master=self._root)
+        self._frame1 = ttk.Frame(self._notebook)
+        self._frame2 = ttk.Frame(self._notebook)
 
-        heading_label = ttk.Label(master=self._frame, text="Lukupäiväkirja")
+        heading_label1 = ttk.Label(master=self._frame1, text="Lukupäiväkirja")
 
-        button = ttk.Button(
-                    master=self._frame,
+        button1 = ttk.Button(
+                    master=self._frame1,
                     text="Lisää luettu kirja",
                     command=self._handle_add_read_book_view)
 
-        label = ttk.Label(master=self._frame, text="Luetut kirjat:")
+        label1 = ttk.Label(master=self._frame1, text="Luetut kirjat:")
         books = book_journal_service.find_all_books()
         book_messages = [f"{book.date}: {book.author}: " \
                         f"{book.title}, {book.pages} sivua.\n " \
                         f"Muistiinpanot: {book.notes}"
                         for book in books]
 
-        heading_label.grid(padx=5, pady=5)
-        button.grid(padx=5, pady=5)
-        label.grid(padx=5, pady=5)
+        heading_label1.grid(padx=5, pady=5)
+        button1.grid(padx=5, pady=5)
+        label1.grid(padx=5, pady=5)
 
-        for message in book_messages:
-            book_list = ttk.Label(master=self._frame, text=message)
+        for message in reversed(book_messages):
+            book_list = ttk.Label(master=self._frame1, text=message)
             book_list.grid(padx=5, pady=5)
+
+        heading_label2 = ttk.Label(master=self._frame2, text="Lukulista")
+
+        button2 = ttk.Button(
+                    master=self._frame2,
+                    text="Lisää kirja lukulistalle")
+
+        label2 = ttk.Label(master=self._frame2, text="Lukulista:")
+
+        heading_label2.grid(padx=5, pady=5)
+        button2.grid(padx=5, pady=5)
+        label2.grid(padx=5, pady=5)
